3fa1bd6704edc916da668f2589ae6636a0bf33be
Luis Castillo <>
2023-01-02 19:40:03 -0600

    Last modifications

--- modules/ec2/create-delete-ASG/main.tf
+++ modules/ec2/create-delete-ASG/main.tf
@@ -123,14 +123,13 @@ resource "aws_launch_template" "app_lt" {
     instance_type = local.config[var.environment]["inst_type"]
 
     network_interfaces {
-        associate_public_ip_address = false
+        associate_public_ip_address = true
         security_groups = [aws_security_group.asg_sg.id]
         subnet_id = local.config[var.environment]["subnets"][0]
     }
 
     tag_specifications {
         resource_type = "instance"
-
         tags = {
         Name = "${var.app_name}"
         }
